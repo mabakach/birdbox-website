bf99232fc113c5120ea5a7fd0b2a112fbd614b70
Marc Baumgartner <marc@mabaka.ch>
2025-04-29 17:24:21 +0200

    Project created

---
+++ src/main/java/ch/mabaka/birdbox_website/BirdboxWebsiteApplication.java
@@ -0,0 +1,13 @@
+package ch.mabaka.birdbox_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdboxWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdboxWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/mabaka/birdbox_website/BirdboxWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.mabaka.birdbox_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdboxWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
