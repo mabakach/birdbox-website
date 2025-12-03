bfbc17cf44a5f0701476ef3031f4f463c70627d7
Marc Baumgartner <marc@mabaka.ch>
2025-12-03 18:41:59 +0100

    Annotate JSON properties explicitly to avoid mapping errors

--- src/main/java/ch/mabaka/birdbox/website/logic/temperature/SensorResponse.java
+++ src/main/java/ch/mabaka/birdbox/website/logic/temperature/SensorResponse.java
@@ -1,10 +1,16 @@
 package ch.mabaka.birdbox.website.logic.temperature;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.Instant;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SensorResponse {
+  @JsonProperty("temperature")
   private double temperature;
+  @JsonProperty("humidity")
   private double humidity;
+  @JsonProperty("readTimestamp")
   private Instant readTimestamp;
 
   public double getTemperature() {
