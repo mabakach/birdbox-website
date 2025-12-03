a9f17aa91cb9a0e2e519b3c260c39d0cd07b2904
Marc Baumgartner <marc@mabaka.ch>
2025-12-03 21:20:38 +0100

    Fix temperature update

--- src/main/java/ch/mabaka/birdbox/website/util/VersionInfo.java
+++ src/main/java/ch/mabaka/birdbox/website/util/VersionInfo.java
@@ -1,13 +1,35 @@
 package ch.mabaka.birdbox.website.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 public class VersionInfo {
+    private static String cachedVersion = null;
+
     public static String getImplementationVersion() {
-        Package pkg = VersionInfo.class.getPackage();
-        if (pkg != null) {
-            String version = pkg.getImplementationVersion();
-            if (version != null) {
-                return version;
+        if (cachedVersion != null) {
+            return cachedVersion;
+        }
+        try {
+            Enumeration<URL> resources = VersionInfo.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                URL url = resources.nextElement();
+                try (InputStream is = url.openStream()) {
+                    Manifest manifest = new Manifest(is);
+                    Attributes attr = manifest.getMainAttributes();
+                    String version = attr.getValue("Implementation-Version");
+                    if (version != null && !version.isEmpty()) {
+                        cachedVersion = version;
+                        return version;
+                    }
+                }
             }
+        } catch (IOException e) {
+            // ignore, fall through to unknown
         }
         return "unknown";
     }
